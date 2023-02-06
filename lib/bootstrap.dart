68a6326793107821f52fcca3da250e9d56b1a64a
Sailesh Dahal <saileshbro@gmail.com>
2023-02-06 14:41:09 +0545

    feat: add flavors to android

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -1,21 +1,24 @@
 import 'package:fitness_app/pages/homepage.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
-void main() {
-  SystemChrome.setSystemUIOverlayStyle(
-    const SystemUiOverlayStyle(statusBarColor: Colors.transparent),
-  );
-  runApp(const MyApp());
-}
+void bootstrap(String env) => runApp(MyApp(env: env));
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({super.key, required this.env});
+  final String env;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      builder: (context, child) {
+        if (env == 'production') return child!;
+        return Banner(
+          message: env.toUpperCase(),
+          location: BannerLocation.topEnd,
+          child: child,
+        );
+      },
       theme: ThemeData(
         fontFamily: 'Montserrat',
       ),
